07fc784d5224e9baf4068385ab64aca39567dc71
HeyGab <gabrielsc031@gmail.com>
2023-09-11 11:36:13 -0300

    Principios de JavaScript

---
+++ js/teste.js
@@ -0,0 +1,6 @@
+function chamarteste(){
+var n1 = Number(prompt('Digite um numero'));
+var n2 = Number.parseInt(prompt('Digite outro numero'));
+var soma = n1 + n2;
+alert("A soma de " + n1 + " e " + n2 + " é: " + soma);
+}
